04b5b1d1b69d2735983bd0dd2cbc7802b1530bdd
Trevor Philip <nl10252@umbc.edu>
2018-04-12 20:11:39 -0400

    First commit for Incremental Development

---
+++ userland/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Name: Trevor Philip
+ * Date: April 11, 2018
+ * 
+ */
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntrusionMonitor
+{
+    class MainClass
+    {
+        private static Queue<>
+        
+        static void Main(string[] args)
+        {
+            // start watching the log directory
+            WatchLogDirectory("/var/log/proj2");
+
+            while (true)
+            {
+                // keep running the main thread, sleep for 50 seconds
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+
+        static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            
+        }
+
+
+        static void EnqueueThreadSafe()
+        {
+            var obj = new Object();
+            lock(obj) {
+                
+            }
+        }
+
+
+        /// <summary>
+        /// Watches the log directory.
+        /// </summary>
+        /// <param name="path">Path.</param>
+        private static void WatchLogDirectory(string path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "*.log";
+            watcher.Changed += Watcher_Changed;
+            watcher.EnableRaisingEvents = true;
+        }
+    }
+}
---
+++ userland/SystemCall.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Name: Trevor Philip
+ * Date: April 11, 2018
+ * Course: CMSC 421 Spring 2018, Project 2
+ * Userspace program
+ */
+
+namespace IntrusionMonitor
+{
+    public class SystemCall
+    {
+        public int Id { get; private set;  }
+        public bool Bit { get; private set; }
+
+        public SystemCall(int id, bool bit)
+        {
+            this.Id = id;
+            this.Bit = bit;
+        }
+    }
+}
